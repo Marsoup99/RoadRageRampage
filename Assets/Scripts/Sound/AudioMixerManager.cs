d01f0a22f2dc70c8fb87c0221fa78922c424943e
Marsoup99 <nd.hoang484@gmail.com>
2023-07-01 22:23:58 +0700

    change audio save way

--- Assets/Scripts/Sound/AudioMixerManager.cs
+++ Assets/Scripts/Sound/AudioMixerManager.cs
@@ -9,33 +9,40 @@ public class AudioMixerManager : MonoBehaviour
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private Slider master, sfx, music; 
     [SerializeField] private GameObject SettingPanelGO;
+    private float master_v, sfx_v, bg_v;
     void Start()
     {
         master.value = PlayerPrefs.GetFloat("MasterVolume", 1);
+        master_v = master.value;
         ChangeMasterVolume(master.value);
         
         sfx.value = PlayerPrefs.GetFloat("SFXVolume", 1);
+        sfx_v = sfx.value;
         ChangeSFXvolume(sfx.value);
 
         music.value = PlayerPrefs.GetFloat("BGVolume", 1);
+        bg_v = music.value;
         ChangeMusicVolume( music.value);
     }
     public void ChangeMasterVolume(float v)
     {
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(v) * 20f);
-        PlayerPrefs.SetFloat("MasterVolume", v);
+        master_v = v;
+        // PlayerPrefs.SetFloat("MasterVolume", v);
     }
 
     public void ChangeSFXvolume(float v)
     {
         audioMixer.SetFloat("SFXVolume", Mathf.Log10(v) * 20f);
-        PlayerPrefs.SetFloat("SFXVolume", v);
+        sfx_v = v;
+        // PlayerPrefs.SetFloat("SFXVolume", v);
     }
 
     public void ChangeMusicVolume(float v)
     {
         audioMixer.SetFloat("BGVolume", Mathf.Log10(v) * 20f);
-        PlayerPrefs.SetFloat("BGVolume", v);
+        bg_v = v;
+        // PlayerPrefs.SetFloat("BGVolume", v);
     }
 
     public void OpenSetting()
@@ -52,6 +59,10 @@ public class AudioMixerManager : MonoBehaviour
 
     public void CloseSetting()
     {
+        PlayerPrefs.SetFloat("MasterVolume", master_v);
+        PlayerPrefs.SetFloat("SFXVolume", sfx_v);
+        PlayerPrefs.SetFloat("BGVolume", bg_v);
+        
         TimeEffectControl.DoNormalTime();
         SettingPanelGO.SetActive(false);
 
